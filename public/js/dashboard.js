386b988897101a4021f53d8a5d16a1cf0cdd1fc4
sagar <sagarinnovate@gmail.com>
2024-11-28 09:23:21 +0530

    partial new ui

--- public/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const token = localStorage.getItem('token');
-  
-    if (!token) {
-      alert('You need to log in first!');
-      window.location.href = 'login.html';
-    }
-  });
-  
