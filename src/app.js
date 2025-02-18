0b8640b169bd26fa80452eee3bd73318f0531576
sagar <sagarinnovate@gmail.com>
2025-02-18 12:54:02 +0530

    changed express-session to express-cookie

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
-const session = require('express-session');
+const session = require('cookie-session');
 require('dotenv').config(); // Load environment variables
 const flash = require('connect-flash');
 
